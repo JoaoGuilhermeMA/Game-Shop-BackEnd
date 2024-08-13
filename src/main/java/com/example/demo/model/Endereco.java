6185e88b597b63d876a0e330ac86e673c5a96305
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-08-13 16:38:06 -0300

    Adicionando o model de Endereço

---
+++ src/main/java/com/example/demo/model/Endereco.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua;
+    private String complemento;
+    private String numero;
+    private String cidade;
+    private String bairro;
+    private String estado;
+    private String pais;
+}
+
