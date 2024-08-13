98965017395361a196d10f62918beb69b60f98b8
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-08-13 16:38:57 -0300

    Adicionando o model de Usuario

---
+++ src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id")
+    private Endereco endereco;
+
+    private String telefone;
+    private String role;
+
+}
+
