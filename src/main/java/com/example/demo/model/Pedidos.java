1ee419e69859a32f70b4fe3fe082d08601b88294
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-08-13 16:38:24 -0300

    Adicionando o model de Pedidos

---
+++ src/main/java/com/example/demo/model/Pedidos.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+public class Pedidos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "pedido_jogo",
+            joinColumns = @JoinColumn(name = "pedido_id"),
+            inverseJoinColumns = @JoinColumn(name = "jogo_id")
+    )
+    private List<Jogo> jogos;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date data;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    private Double preco;
+}
